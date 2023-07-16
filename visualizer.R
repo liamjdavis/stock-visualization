b9c37ec06bb571e0bf8fc3f0a9cb405466734291
liamjdavis <66973153+liamjdavis@users.noreply.github.com>
2023-07-16 16:20:36 -0700

    Add files via upload

--- visualizer.R
+++ visualizer.R
@@ -1,6 +1,14 @@
-# download stock prices
 install.packages("quantmod")
 library(quantmod)
 
-data()
+install.packages("ggplot2")
+library(ggplot2)
+
+# stocks to visualize
+stocks = c("AAPL", "NFLX", "AMZN", "TSLA", "GOOGL")
+
+# visualize for past year
+currDate = Sys.Date()
+startDate = currDate %m-% years(1)
+getSymbols(stocks, from = startDate, to = currDate, auto.assign = TRUE)
 
